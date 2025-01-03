f725964cdb68952971a377bff06d3593d7077faa
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2025-01-03 15:45:21 +0100

    Initialised a dotnet 9 console application and started working on the logic

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var httpClient = new HttpClient() { };
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/download", async (string[] urls) =>
+{
+  var regex = new Regex(@"^https:\/\/images\.unsplash\.com\/photo-\w+-\w+\?");
+  var filtered = urls.Where(url => regex.Match(url).Success);
+
+  try
+  {
+    var photosByteArrays = await Task.WhenAll(filtered.Select(async url => await httpClient.GetByteArrayAsync(url)));
+    var randomName = RandomNumberGenerator.GetHexString(10);
+
+    if (photosByteArrays != null)
+    {
+      for (int i = 0; i < photosByteArrays.Length; i++)
+      {
+        if (photosByteArrays[i] != null)
+        {
+          var basePath = $"downloaded/{randomName}";
+          Directory.CreateDirectory(basePath);
+
+          await File.WriteAllBytesAsync($"{basePath}/photo{i}.jpeg", photosByteArrays[i]);
+        }
+      }
+    }
+  }
+  catch { }
+});
+
+app.Run();
+
+/*
+Example request body:
+
+[
+  "https://images.unsplash.com/photo-1730119986244-eb33b57b3950?ixid=M3w2NzYxNTh8MHwxfGFsbHwxfHx8fHx8fHwxNzM1ODUwMjc0fA&ixlib=rb-4.0.3",
+  "https://images.unsplash.com/photo-1729877251622-a9043a2ec183?ixid=M3w2NzYxNTh8MHwxfGFsbHwyfHx8fHx8fHwxNzM1ODUwMjc0fA&ixlib=rb-4.0.3",
+  "https://images.unsplash.com/photo-1723654864018-36cc407e4a69?ixid=M3w2NzYxNTh8MHwxfGFsbHwzfHx8fHx8fHwxNzM1ODUwMjc0fA&ixlib=rb-4.0.3",
+  "https://images.unsplash.com/photo-1731412924028-204b15ca8f1d?ixid=M3w2NzYxNTh8MHwxfGFsbHw4fHx8fHx8fHwxNzM1ODUwMjc0fA&ixlib=rb-4.0.3",
+  "https://images.unsplash.com/photo-1731450453063-31716e7b71af?ixid=M3w2NzYxNTh8MHwxfGFsbHw5fHx8fHx8fHwxNzM1ODUwMjc0fA&ixlib=rb-4.0.3"
+]
+*/
